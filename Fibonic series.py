73ace596c5ef0c119e29f66ea5aff7b038e6d162
Keshav Anand <120261440+anandKeshav001@users.noreply.github.com>
2023-01-17 23:37:43 +0530

    Add files via upload

---
+++ Fibonic series.py
@@ -0,0 +1,34 @@
+# L = []
+# for i in range(int(input("How many digits you type:--"))):
+#     i = int(input("--"))
+#     L.append(i)
+# hi = max(L)
+# # print(L)
+# x = int(hi)
+# a = 0
+# b = 1
+# z = 0
+# l = []
+# if x <=0:
+#     print("only gives postive values")
+# elif x == 1:
+#     print(a)
+# else:
+#     while z < x:
+#         l.append(a)
+#         A = a+b
+#         a = b
+#         b = A
+#         z = z+1
+#     # print(l)
+#     length = len(L)
+#     for j in range(length):
+#         if L[j] in l:
+#             print("valid", L[j])
+#         elif L[j] not in l:
+#             print("invalid", L[j])
+
+a=input()
+print(a ,type(a))
+b=int(a)
+print(b,type(b))
